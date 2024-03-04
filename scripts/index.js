79eb62d5826b116d8b348e00247cc04f328be8b7
NahiyaNasir <nahiaesha.01@gmail.com>
2024-03-04 16:40:20 +0600

    code modefied

--- scripts/index.js
+++ scripts/index.js
@@ -12,21 +12,60 @@ Example
 PostByQuery: - https://openapi.programming-hero.com/api/retro-forum/posts?category=coding
 */
 
+const loadAllPost=async()=>{
+     const res=await fetch('https://openapi.programming-hero.com/api/retro-forum/posts')
+     const data=await res.json()
+     const allPost=data.posts
+     console.log(allPost)
+
+     allPost.forEach(item=>{
+  const cardContainer=document.getElementById('card-container')  
+  const postDiv= document.createElement('div')
+        postDiv.classList=`card card-side bg-[#F3F3F5] shadow-xl  gap-8 mb-6 `
+        postDiv.innerHTML=`
+                <div class=" w-3 h-3 rounded-full absolute top-4 left-28       ${item.isActive? "bg-green-700":"bg-red-500"}    ">
+               
+                </div>
+          <div class=" p-6 rounded-2xl"> <figure class="w-[100px] h-[100px]"><img src="${item.image}" alt="Movie"  /></figure></div>
+        <div class="card-body">
+        <div class=" flex   justify-center ">
+  <h1 class="text-center"> #${item.category}</h1>
+ <p class="text-center">  Author:${ item.author.name}</p></div>
+          <h2 class="card-title">${item.title}</h2>
+          <p>${item.description}.</p>
+          <div class="card-actions justify-between">
+          <p><i class="fa-regular fa-message"></i> ${item.comment_count}</p>
+          <p><i class="fa-regular fa-eye"></i>${item.view_count}</p>
+        <p><i class="fa-regular fa-clock"></i>${item.posted_time} min</p>
+        <button class="btn btn-primary rounded-full" id=" envelope" onclick="getData('${item.title.replace(/'/g,"")}' ,'${item.view_count}')"><i class="fa-regular fa-envelope"></i></button>
+          </div>
+        </div>
+      </div>
+       
+      
+        `
+      //  console.log(post.title)
+      //  console.log(post.view_count)
+      cardContainer.appendChild(postDiv)
+     })
+}
+//load post & display
  const loadPost=async(categoryName)=>{
     const res= await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${categoryName}`)
     const data= await res.json()
     const posts=data.posts
-    console.log(posts)
+   // console.log(posts)
     displayPosts(posts)
+    
  }
     const displayPosts=(posts)=>{
       const cardContainer=document.getElementById('card-container')  
       cardContainer.textContent=''  
       posts.forEach(post=>{
       // cardContainer.textContent=''
-        console.log(post)   
+   //     console.log(post)   
         const postDiv= document.createElement('div')
-        postDiv.classList=`card card-side bg-[#F3F3F5] shadow-xl w-1/2 gap-8 mb-6 `
+        postDiv.classList=`card card-side bg-[#F3F3F5] shadow-xl   gap-8 mb-6 `
         postDiv.innerHTML=`
                 <div class=" w-3 h-3 rounded-full absolute top-4 left-28       ${post.isActive? "bg-green-700":"bg-red-500"}    ">
                
@@ -34,7 +73,7 @@ PostByQuery: - https://openapi.programming-hero.com/api/retro-forum/posts?catego
           <div class=" p-6 rounded-2xl"> <figure class="w-[100px] h-[100px]"><img src="${post.image}" alt="Movie"  /></figure></div>
         <div class="card-body">
         <div class=" flex   justify-center ">
-  <h1 class="text-center"> #${post.category}t</h1>
+  <h1 class="text-center"> #${post.category}</h1>
  <p class="text-center">  Author:${ post.author.name}</p></div>
           <h2 class="card-title">${post.title}</h2>
           <p>${post.description}.</p>
@@ -42,7 +81,7 @@ PostByQuery: - https://openapi.programming-hero.com/api/retro-forum/posts?catego
           <p><i class="fa-regular fa-message"></i> ${post.comment_count}</p>
           <p><i class="fa-regular fa-eye"></i>${post.view_count}</p>
         <p><i class="fa-regular fa-clock"></i>${post.posted_time} min</p>
-        <button class="btn btn-primary rounded-full" id=" envelope" onclick="getData('${post.title}' ,'${post.view_count}')"><i class="fa-regular fa-envelope"></i></button>
+        <button class="btn btn-primary rounded-full" id=" envelope" onclick="getData('${post.title.replace(/'/g,"")}' ,'${post.view_count}')"><i class="fa-regular fa-envelope"></i></button>
           </div>
         </div>
       </div>
@@ -53,30 +92,42 @@ PostByQuery: - https://openapi.programming-hero.com/api/retro-forum/posts?catego
       //  console.log(post.view_count)
       cardContainer.appendChild(postDiv)
       })
+      //hide 
+      toggledSpinner(false)
   }
     
    //displayPost()
 //display data end
   
 const getData=async(title,view  )=>{
-  //console.log('click',read)
-  let read=1
-  let count=document.getElementById('read')
-  count=read+count
- read++
-     const readContainer=document.getElementById('read-Container')
+  console.log('click')
+  let markAsRead=document.getElementById('read')
+  console.log(markAsRead)
+  let count=0
+  
+  
+ 
+ 
+ 
+     const readContainer=document.getElementById('read-container')
     
         const titleDiv=document.createElement('div')
-       titleDiv.classList=`card card-side bg-[#F3F3F5] shadow-xl w-1/2 gap-8 mb-6  p-6`
+        //let markAsRead=document.getElementById('read')
+       titleDiv.classList=`card card-side bg-[#F3F3F5] shadow-xl  gap-8 mb-6  p-6`
          titleDiv.innerHTML=`
-         <div>
+         <div class="flex  gap-8">
          <h1 class=" font-medium">${title}</h1>
          <p><i class="fa-regular fa-eye"></i>${view}</p>
        </div>
          
          `
         readContainer.appendChild(titleDiv) 
+        count++
+      `  markAsRead(${count})`
+        
+
 }
+
   // latest posts
  const loadLatestPost= async()=>{
   const res= await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts')
@@ -89,7 +140,7 @@ const getData=async(title,view  )=>{
     card.classList=`card w-96 bg-base-100 shadow-xl`
     card.innerHTML=`
     <figure class="overflow-hidden h-72">
-    <img class="w-full" src="${item.cover_image}" alt="Shoes" />
+    <img class="w-full rounded-xl p-3" src="${item.cover_image}" alt="Shoes" />
     <h6 class="absolute bottom-[40%] right-12"></h6>
 </figure>
 <div class="card-body">
@@ -119,21 +170,33 @@ const getData=async(title,view  )=>{
   })
  }
 
-
-
-
 // handel show search
-  const  handelSearch= ( )=>{
-    const searchInput=document.getElementById('search-input').value
-   const searchText=searchInput
-   console.log(searchText)
+const  handelSearch= ( )=>{
+ toggledSpinner(true)
+  const searchInput=document.getElementById('search-input').value
+ const searchText=searchInput
+ console.log(searchText)
+ 
+ loadPost(searchText)
 
-   loadPost(searchText)
-         
+     //  getData()
+
+
+
+}
+ // load spinner
+   const toggledSpinner=(isLoading)=>{
+    const spinner=document.getElementById('loading-spinner')
+    //spinner.classList.remove('hidden')
+ if(isLoading){
+  spinner.classList.remove('hidden')
+ }else{
+    spinner.classList.add('hidden')
+ }
+   }
+
 
   
-
-  }
   
 
 
@@ -144,8 +207,9 @@ loadLatestPost()
 
 
 
-getData()
- loadPost('comedy')
+
+loadAllPost()
+// loadPost('comedy')
 
 
 
